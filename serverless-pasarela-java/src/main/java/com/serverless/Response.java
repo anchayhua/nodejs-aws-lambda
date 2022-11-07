b2dc045a11692fddb5861ad3b7a990d665e923c7
Nicanor <nanchayhua@vivienda.gob.pe>
2022-11-07 17:25:31 -0500

    serverless con java para pasarela culqi

---
+++ serverless-pasarela-java/src/main/java/com/serverless/Response.java
@@ -0,0 +1,13 @@
+// package serverless-pasarela-java.src.main.java.com.serverless;
+
+public class Response {
+    private final String message;
+
+    public Response(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
---
+++ serverless-pasarela-java/src/main/java/com/serverless/procesarcargo.java
@@ -0,0 +1,27 @@
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Procesarcargo implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+	private static final Logger LOG = Logger.getLogger(Handler.class);
+
+	@Override
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		LOG.info("received: " + input);
+		Response responseBody = new Response("Metodo para procesar Cargo " + new Date());
+		Map<String, String> headers = new HashMap<>();
+		headers.put("X-Powered-By", "AWS Lambda & Serverless");
+		headers.put("Content-Type", "application/json");
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(headers)
+				.build();
+	}
+}
